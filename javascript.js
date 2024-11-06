e88c4ae29f14d69bfa75ba2ace2927e5bb185007
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-11-05 21:16:46 -0800

    Create functionality to compute arithmetic operations, and store rolling calculations

--- javascript.js
+++ javascript.js
@@ -2,24 +2,37 @@ let order = "firstNumber";
 let firstNumber = "";
 let secondNumber = "";
 let displayReturn = "";
+let functionCall = "";
 
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
 
 function add(a,b) {
-    return a + b;
+    a = Number(a);
+    b = Number(b);
+    console.log(a+b +"add");
+    displayReturn = a + b;
 }
 
 function subtract(a,b) {
-    return a - b;
+    a = Number(a);
+    b = Number(b);
+    console.log(a-b + "subtract");
+    displayReturn = a - b;
 }
 
 function divide(a,b) {
-    return a / b;
+    a = Number(a);
+    b = Number(b);
+    console.log(a/b + "divide");
+    displayReturn = a / b;
 }
 
 function multiply(a,b) {
-    return a * b;
+    a = Number(a);
+    b = Number(b);
+    console.log(a*b + "multiply");
+    displayReturn = a * b;
 }
 
 function allClear() {
@@ -27,6 +40,20 @@ function allClear() {
     secondNumber = "";
     displayReturn = 0;
     order = "firstNumber";
+}
+
+function clear() {
+    if (order == "firstNumber") {
+        firstNumber = "";
+    } else if (order == "secondNumber") {
+        secondNumber = ""
+    }
+}
+
+function multiCalculation () {
+    firstNumber = displayReturn;
+    secondNumber = "";
+    order = "secondNumber";
 }
 
 const numberElements = document.querySelectorAll(".number");
@@ -37,6 +64,7 @@ numberElements.forEach((key) => {
         console.log(order);
         if (order == "firstNumber") {
             firstNumber += input.textContent;
+            displayReturn = firstNumber;
         } else if (order == "secondNumber") {
             secondNumber += input.textContent;
         }
@@ -56,38 +84,75 @@ operationElements.forEach((key) => {
             case "allClear":
                 allClear();
                 break;
+            case "clear":
+                clear();
+                break;
             case "add":
-                if (!secondNumber == "") {
+                if (secondNumber) {
                     add(firstNumber,secondNumber);
+                    multiCalculation();
                 } else {
                     order = "secondNumber";
+                    functionCall = "add";
                 }
                 break;
             case "subtract":
-                if (!secondNumber == "") {
+                if (secondNumber) {
                     subtract(firstNumber,secondNumber);
+                    multiCalculation();
                 } else {
                     order = "secondNumber";
+                    functionCall = "subtract";
                 }
+
                 break;
             case "divide":
-                if (!secondNumber == "") {
+                if (secondNumber) {
                     divide(firstNumber,secondNumber);
+                    multiCalculation();
                 } else {
                     order = "secondNumber";
+                    functionCall = "divide";
                 }
                 break;
             case "multiply":
-                if (!secondNumber == "") {
+                if (secondNumber) {
                     multiply(firstNumber,secondNumber);
+                    multiCalculation();
                 } else {
                     order = "secondNumber";
+                    functionCall = "multiply";
                 }
                 break;
+            case "equal":
+                switch (functionCall) {
+                    case "add": 
+                        add(firstNumber,secondNumber);
+                        break;
+                    case "subtract":
+                        subtract(firstNumber,secondNumber);
+                        break;
+                    case "multiply":
+                        multiply(firstNumber,secondNumber);
+                        break;
+                    case "divide":
+                        divide(firstNumber,secondNumber);
+                        break;
+                    default:
+                        
+                }
+                multiCalculation();
+
             default:
                 displayReturn = "Error";
         }
     event.stopPropagation();
-    })
-})
+    });
+});
 
+const display = document.querySelector("#display");
+
+display.addEventListener("click", (event) => {
+    display.textContent = displayReturn;
+    event.stopPropagation();
+});
